cb65d18cbe943abc65468614a5a72fd5f701eb09
jraivio <juraivio@gmail.com>
2020-12-22 22:44:09 +0200

    Changes to be committed: 	modified:   create-nsciotservice.sh 	new file:   docker-compose-example.yml 	new file:   docker-compose-footer.yml 	new file:   docker-compose-header.yml 	new file:   iotservice.properties-template

--- create-nsciotservice.sh
+++ create-nsciotservice.sh
@@ -1,16 +1,40 @@
 #!/bin/bash
+# Move old files
+mv docker-compose.yml docker-compose.old
 # Attach iot-key file name
 for keyfile in `ls ./iotkey`; do
 export iotkey=$keyfile
 done
-#if keyfile=NULL then echo "Iot key is missing!"; exit
-export contid="1"
-export ipnumber="172.21.0.2"
-export uuid=$(uuidgen)
-rm -f docker-compose.yml temp.yml     
-( echo "cat <<EOF >docker-compose.yml";
+echo "Number of video streams (^C to interrupt):"
+declare -i STREAMS
+read STREAMS
+declare -i i=1
+until test $i -gt $STREAMS
+do
+  echo "RTSP url address for video stream number $i:"
+  read RTSP$i
+  export RTSPurl=RTSP$i
+  export contid=$i
+  declare -i ip
+  ip=$(( $i + 1 ))
+  export ipnumber="172.21.0.$ip"
+  export uuid=$(uuidgen)
+  # create containers
+  ( echo "cat <<EOF >docker-compose-temp$contid.yml";
   cat docker-compose-template.yml;
-  echo "EOF";
-) >temp.yml
-. temp.yml
-cat docker-compose.yml
+  ) >temp.yml
+  . temp.yml
+  cat docker-compose-temp$contid.yml > docker-compose-temp.yml;
+  cat docker-compose-temp.yml >> docker-compose-containers.yml;
+  rm -f docker-compose-temp$contid.yml temp.yml docker-compose-temp.yml;
+  # create video input configuration
+  ( echo "cat <<EOF >iotconf$contid";
+  cat iotservice.properties-template;
+  ) >temp
+  . temp
+  cat iotconf$contid > ./nscIotConfig/iotservice.properties$contid
+  rm -f iotconf$contid temp
+  i=$(( $i + 1 ))
+done
+cat docker-compose-header.yml docker-compose-containers.yml docker-compose-footer.yml > docker-compose.yml
+rm -f docker-compose-containers.yml
