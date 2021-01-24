d77d37bde445c1359342d54fc4b6b8273c6a45b8
Jukka Raivio <juraivio@gmail.com>
2021-01-24 08:32:37 +0200

    Update create-nsciotservice.sh

--- create-nsciotservice.sh
+++ create-nsciotservice.sh
@@ -30,12 +30,12 @@ then
 fi
 if [ $HWCONF -gt 3 ]
 then
-    echo "Selected value not in range 1-3"
+    echo "Selected value $HWCONF is out of range 1-3!"
 exit 0
 fi
 if [ $HWCONF -lt 1 ]
 then
-    echo "Selected value not in range 1-3"
+    echo "Selected value $$HWCONF is out of range 1-3!"
 exit 0
 fi
 echo ""
